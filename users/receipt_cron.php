288459b93d61f83254a2372103d2e3b22eb4506d
Yusuf Rampurawala <yusuf4u52@gmail.com>
2021-04-02 19:55:18 +0530

    remove cancelled receipts

--- users/receipt_cron.php
+++ users/receipt_cron.php
@@ -6,43 +6,43 @@ date_default_timezone_set('Asia/Kolkata');
 $counter = 1;
 $file = fopen("fmbreceipts.csv", "r");
 while (($column = fgetcsv($file)) !== FALSE) {
+    if ($column[13] != "Cancelled") {
+        if ($counter != 1) {
+            $receiptno = "";
+            if (isset($column[0])) {
+                $receiptno = mysqli_real_escape_string($link, $column[0]);
+            }
+            $thalino = "";
+            if (isset($column[1])) {
+                $thalino = mysqli_real_escape_string($link, $column[1]);
+            }
+            $amount = "";
+            if (isset($column[4])) {
+                $amount = mysqli_real_escape_string($link, $column[4]);
+                $amount = str_replace('₹', '', $amount);
+                $amount = str_replace(',', '', $amount);
+                $amount = intval($amount);
+            }
+            $date = "";
+            if (isset($column[3])) {
+                $datefromcsv = mysqli_real_escape_string($link, $column[3]);
+                $datestring = getHijriDate($datefromcsv);
+            }
+            $receivedby = "";
+            if (isset($column[10])) {
+                $receivedby = mysqli_real_escape_string($link, $column[10]);
+            }
 
-    if ($counter != 1) {
-        $receiptno = "";
-        if (isset($column[0])) {
-            $receiptno = mysqli_real_escape_string($link, $column[0]);
-        }
-        $thalino = "";
-        if (isset($column[1])) {
-            $thalino = mysqli_real_escape_string($link, $column[1]);
-        }
-        $amount = "";
-        if (isset($column[4])) {
-            $amount = mysqli_real_escape_string($link, $column[4]);
-            $amount = str_replace('₹', '', $amount);
-            $amount = str_replace(',', '', $amount);
-            $amount = intval($amount);
-        }
-        $date = "";
-        if (isset($column[3])) {
-            $datefromcsv = mysqli_real_escape_string($link, $column[3]);
-            $datestring = getHijriDate($datefromcsv);
-        }
-        $receivedby = "";
-        if (isset($column[10])) {
-            $receivedby = mysqli_real_escape_string($link, $column[10]);
-        }
+            $sql = "select NAME,id from thalilist WHERE thali = '$thalino'";
+            $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+            $name = mysqli_fetch_assoc($result);
 
-        $sql = "select NAME,id from thalilist WHERE thali = '$thalino'";
-        $result = mysqli_query($link, $sql) or die(mysqli_error($link));
-        $name = mysqli_fetch_assoc($result);
-
-        $sqlInsert = "replace into receipts (`Receipt_No`,`Thali_No`,`userid`, `name`, `Date`,`Amount`,`received_by`)
+            $sqlInsert = "replace into receipts (`Receipt_No`,`Thali_No`,`userid`, `name`, `Date`,`Amount`,`received_by`)
                    values ('$receiptno', '$thalino', '" . $name['id'] . "', '" . $name['NAME'] . "', '$datestring', '$amount', '$receivedby')";
-        mysqli_query($link, $sqlInsert) or die(mysqli_error($link));
-        
+            mysqli_query($link, $sqlInsert) or die(mysqli_error($link));
+        }
+        $counter++;
     }
-    $counter++;
 }
 echo "Success\n";
 ?>
