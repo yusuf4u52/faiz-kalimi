eba781879e38caaec803dd418697cb2c19f02975
Yusuf Rampurawala <yusuf4u52@gmail.com>
2021-04-04 10:45:52 +0530

    Relative path changes again

--- admin/application/views/example.php
+++ admin/application/views/example.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<?php 
-foreach($css_files as $file): ?>
-	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
-<?php endforeach; ?>
-<?php include('../users/_head.php'); ?>
-</head>
-<body>
-<?php include('../users/_nav.php'); ?>
-	<div style='height:20px;'></div>  
-    <div style="padding: 10px">
-		<?php echo $output; ?>
-    </div>
-    <?php foreach($js_files as $file): ?>
-        <script src="<?php echo $file; ?>"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php
+	foreach ($css_files as $file) : ?>
+		<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
 	<?php endforeach; ?>
-	<script src="../users/javascript/bootstrap-3.3.6.min.js"></script>
+	<?php include('../users/_head.php'); ?>
+</head>
+
+<body>
+	<?php include('../users/_nav.php'); ?>
+	<div style='height:20px;'></div>
+	<div style="padding: 10px">
+		<?php echo $output; ?>
+	</div>
+	<?php foreach ($js_files as $file) : ?>
+		<script src="<?php echo $file; ?>"></script>
+	<?php endforeach; ?>
+	<script src="/fmb/users/javascript/bootstrap-3.3.6.min.js"></script>
 
 </body>
+
 </html>
--- users/_nav.php
+++ users/_nav.php
@@ -13,46 +13,46 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
         <?php if ($_SESSION['role']) { ?>
-          <li><a href="musaid.php">Musaid</a></li>
+          <li><a href="/fmb/users/musaid.php">Musaid</a></li>
         <?php } ?>
         <?php
         if (in_array($_SESSION['email'], array('nationalminerals52@gmail.com', 'mesaifee52@gmail.com', 'murtaza52@gmail.com', 'murtaza.sh@gmail.com', 'yusuf4u52@gmail.com', 'tzabuawala@gmail.com', 'mustafamnr@gmail.com', 'ismailsidhpuri@gmail.com'))) {
         ?>
-          <li><a href="pendingactions.php">Pending Actions</a></li>
-          <li><a href="_daily_hisab_entry.php">Daily Hisab</a></li>
-          <li><a href="thalisearch.php">Thaali Search</a></li>
-          <li><a href="requestarchive.txt">CR NR</a></li>
-          <li><a href="stopMultipleThaalis.php">Stop Thali</a></li>
-          <li><a href="expenses_new.php">Expenses</a></li>
+          <li><a href="/fmb/users/pendingactions.php">Pending Actions</a></li>
+          <li><a href="/fmb/users/_daily_hisab_entry.php">Daily Hisab</a></li>
+          <li><a href="/fmb/users/thalisearch.php">Thaali Search</a></li>
+          <li><a href="/fmb/users/requestarchive.txt">CR NR</a></li>
+          <li><a href="/fmb/users/stopMultipleThaalis.php">Stop Thali</a></li>
+          <li><a href="/fmb/users/expenses_new.php">Expenses</a></li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Backend <span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
-              <li><a href="../admin/index.php/examples/faiz">Admin</a></li>
-              <li><a href="../admin/index.php/examples/receipts">Receipts</a></li>
-              <li><a href="../admin/index.php/examples/change">CR NR</a></li>
-              <li><a href="../admin/index.php/examples/event_response">Event Response</a></li>
-              <li><a href="../admin/index.php/examples/daily_hisab_items">Daily Items</a></li>
-              <li><a href="../admin/index.php/examples/daily_menu_count">Menu-Count</a>
-              <li><a href="../admin/index.php/examples/sf_hisab">SF Purchases</a></li>
-              <li><a href="admin_scripts.php">Scripts</a></li>
+              <li><a href="/fmb/admin/index.php/examples/faiz">Admin</a></li>
+              <li><a href="/fmb/admin/index.php/examples/receipts">Receipts</a></li>
+              <li><a href="/fmb/admin/index.php/examples/change">CR NR</a></li>
+              <li><a href="/fmb/admin/index.php/examples/event_response">Event Response</a></li>
+              <li><a href="/fmb/admin/index.php/examples/daily_hisab_items">Daily Items</a></li>
+              <li><a href="/fmb/admin/index.php/examples/daily_menu_count">Menu-Count</a>
+              <li><a href="/fmb/admin/index.php/examples/sf_hisab">SF Purchases</a></li>
+              <li><a href="/fmb/users/admin_scripts.php">Scripts</a></li>
             </ul>
           </li>
-          <li><a href="notpickedup.php">NotPickedUp</a></li>
-          <li><a target="_blank" href="../sms/index.php">SMS</a></li>
+          <li><a href="/fmb/users/notpickedup.php">NotPickedUp</a></li>
+          <li><a target="_blank" href="/fmb/sms/index.php">SMS</a></li>
         <?php
         }
         ?>
         <?php
         if (in_array($_SESSION['email'], array('yusuf4u52@gmail.com', 'mustafamnr@gmail.com'))) {
         ?>
-          <li><a href="amount_received_by.php">Received</a></li>
+          <li><a href="/fmb/users/amount_received_by.php">Received</a></li>
         <?php
         }
         ?>
-        <li><a href="hoobHistory.php">My Receipts</a></li>
-        <li><a href="update_details.php">Update details</a></li>
-        <li><a href="events.php">Event Registration</a></li>
-        <li><a href="logout.php">Logout</a></li>
+        <li><a href="/fmb/users/hoobHistory.php">My Receipts</a></li>
+        <li><a href="/fmb/users/update_details.php">Update details</a></li>
+        <li><a href="/fmb/users/events.php">Event Registration</a></li>
+        <li><a href="/fmb/users/logout.php">Logout</a></li>
       </ul>
     </div>
   </div>
