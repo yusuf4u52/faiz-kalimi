6a4f5c754a9b02c120dbc414fa2dec8be4070b66
Yusuf Rampurawala <yusuf4u52@gmail.com>
2021-04-08 22:43:40 +0530

    Expenses fetch and integrated

--- users/receipt_cron.php
+++ users/receipt_cron.php
@@ -2,6 +2,21 @@
 include('connection.php');
 include('getHijriDate.php');
 
+$months = array(
+    '09' => 'Ramazan',
+    '10' => 'Shawwal',
+    '11' => 'Zilqad',
+    '12' => 'Zilhaj',
+    '01' => 'Moharram',
+    '02' => 'Safar',
+    '03' => 'RabiulAwwal',
+    '04' => 'RabiulAkhar',
+    '05' => 'JamadalAwwal',
+    '06' => 'JamadalAkhar',
+    '07' => 'Rajab',
+    '08' => 'Shaban'
+);
+
 // reset paid to 0 before updating receipts
 $sql = "UPDATE thalilist set Paid = 0";
 mysqli_query($link, $sql) or die(mysqli_error($link));
@@ -51,4 +66,53 @@ while (($column = fgetcsv($file)) !== FALSE) {
         $counter++;
     }
 }
+
+//update expenses
+$counter = 1;
+$file = fopen("fmbpayments.csv", "r");
+while (($column = fgetcsv($file)) !== FALSE) {
+    if ($column[14] != "Cancelled") {
+        if ($counter != 1) {
+            $voucherno = "";
+            if (isset($column[0])) {
+                $voucherno = mysqli_real_escape_string($link, $column[0]);
+            }
+            $vendor = "";
+            if (isset($column[4])) {
+                $vendor = mysqli_real_escape_string($link, $column[4]);
+            }
+            $amount = "";
+            if (isset($column[5])) {
+                $amount = mysqli_real_escape_string($link, $column[5]);
+                $amount = str_replace('₹', '', $amount);
+                $amount = str_replace(',', '', $amount);
+                $amount = intval($amount);
+            }
+            $date = "";
+            if (isset($column[3])) {
+                $datefromcsv = mysqli_real_escape_string($link, $column[3]);
+                $datestring = date_format(date_create($datefromcsv), "Y-m-d");
+                $datemonth = getHijriMonth($datefromcsv);
+                $datemonth = $months["$datemonth"];
+            }
+            $description = "";
+            if (isset($column[11])) {
+                $description = mysqli_real_escape_string($link, $column[11]);
+            }
+
+            $sql = "select NAME,id from thalilist WHERE thali = '$thalino'";
+            $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+            $name = mysqli_fetch_assoc($result);
+
+            $sqlInsert = "replace into account (`id`,`Date`,`Type`, `Amount`, `Month`,`Remarks`)
+                   values ('$voucherno', '$datestring', '$vendor', '$amount', '$datemonth', '$description')";
+            mysqli_query($link, $sqlInsert) or die(mysqli_error($link));
+
+            $sql = "UPDATE thalilist set Paid = Paid + '$amount' WHERE thali = '$thalino'";
+            mysqli_query($link, $sql) or die(mysqli_error($link));
+        }
+        $counter++;
+    }
+}
+
 echo "Success\n";
