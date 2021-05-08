ce25c79a530a9d2f7916414791dcab84ec31cf95
Yusuf Rampurawala <yusuf4u52@gmail.com>
2021-05-08 11:46:52 +0530

    get balance

--- sms/_getBalance.php
+++ sms/_getBalance.php
@@ -1,5 +1,4 @@
 <?php
 require '_credentials.php';
-$balance = file_get_contents("http://sms1.almasaarr.com/api/balance.php?authkey=$smsauthkey&type=TEMPLATE");
-echo trim($balance);
-?>
+$balance = file_get_contents("https://www.fast2sms.com/dev/wallet?authorization=$smsauthkey");
+echo json_decode($balance)->wallet;
