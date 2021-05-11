6a80970b3754dbbd4f5e9adea5dcf4c692fdfa9d
Yusuf Rampurawala <yusuf4u52@gmail.com>
2021-05-11 22:18:57 +0530

    start stop not allowed post 6pm

--- users/start_thali.php
+++ users/start_thali.php
@@ -5,26 +5,38 @@ include('getHijriDate.php');
 $today = getTodayDateHijri();
 session_start();
 if (is_null($_SESSION['fromLogin'])) {
- //send them back\
-   header("Location: login.php");
+  //send them back\
+  header("Location: login.php");
 }
 
-$query="SELECT * FROM thalilist where id = '".$_SESSION['thaliid']."'";
-$values = mysqli_fetch_assoc(mysqli_query($link,$query));
+// check if request is in cut off time
+date_default_timezone_set('Asia/Kolkata');
+$cutoffTime = '18:00'; //Cut off at 6 pm
+$startTime = '23:59'; //reset back to open at midnight
 
-if($values['hardstop'] == 1) exit;
+$time = new DateTime($cutoffTime);
+$time1 = date_format($time, 'H:i');
+$time = new DateTime($startTime);
+$time2 = date_format($time, 'H:i');
 
-mysqli_query($link,"UPDATE thalilist set Active='1' WHERE id = '".$_SESSION['thaliid']."'") or die(mysqli_error($link));
-mysqli_query($link,"UPDATE thalilist set Thali_start_date='" . $today . "' WHERE id = '".$values['id']."'") or die(mysqli_error($link));
+$current = date("H:i");
+if ($current > $time1 && $current < $time2) {
+  $cutoffmessage =  'Start thali not allowed post 6 PM. Please message 9503054797';
+  header("Location: index.php?status=$cutoffmessage");
+  exit;
+}
+
+$query = "SELECT * FROM thalilist where id = '" . $_SESSION['thaliid'] . "'";
+$values = mysqli_fetch_assoc(mysqli_query($link, $query));
+
+if ($values['hardstop'] == 1) exit;
+
+mysqli_query($link, "UPDATE thalilist set Active='1' WHERE id = '" . $_SESSION['thaliid'] . "'") or die(mysqli_error($link));
+mysqli_query($link, "UPDATE thalilist set Thali_start_date='" . $today . "' WHERE id = '" . $values['id'] . "'") or die(mysqli_error($link));
 
 
-mysqli_query($link,"update change_table set processed = 1 where userid = '" . $_SESSION['thaliid'] . "' and `Operation` in ('Start Thali','Stop Thali') and processed = 0") or die(mysqli_error($link));
-mysqli_query($link,"INSERT INTO change_table (`Thali`, `userid`, `Operation`, `Date`) VALUES ('".$values['Thali']."','".$_SESSION['thaliid']."', 'Start Thali','" . $today . "')") or die(mysqli_error($link));
+mysqli_query($link, "update change_table set processed = 1 where userid = '" . $_SESSION['thaliid'] . "' and `Operation` in ('Start Thali','Stop Thali') and processed = 0") or die(mysqli_error($link));
+mysqli_query($link, "INSERT INTO change_table (`Thali`, `userid`, `Operation`, `Date`) VALUES ('" . $values['Thali'] . "','" . $_SESSION['thaliid'] . "', 'Start Thali','" . $today . "')") or die(mysqli_error($link));
 
 $status = 'Start Thali Successful';
-
 header("Location: index.php?status=$status");
-
-
-
-?>
--- users/stop_thali.php
+++ users/stop_thali.php
@@ -7,16 +7,32 @@ session_start();
 
 if (is_null($_SESSION['fromLogin'])) {
 
- //send them back
-   header("Location: login.php");
+  //send them back
+  header("Location: login.php");
 }
 
-$update = mysqli_query($link,"UPDATE thalilist set Active='0' WHERE id = '".$_SESSION['thaliid']."'") or die(mysqli_error($link));
-$update = mysqli_query($link,"UPDATE thalilist set Thali_stop_date='" . $today . "' WHERE id = '".$_SESSION['thaliid']."'") or die(mysqli_error($link));
+// check if request is in cut off time
+date_default_timezone_set('Asia/Kolkata');
+$cutoffTime = '18:00'; //Cut off at 6 pm
+$startTime = '23:59'; //reset back to open at midnight
 
-mysqli_query($link,"update change_table set processed = 1 where userid = '".$_SESSION['thaliid']."' and `Operation` in ('Start Thali','Stop Thali','Start Transport','Stop Transport') and processed = 0") or die(mysqli_error($link));
-mysqli_query($link,"INSERT INTO change_table (`Thali`, `userid`,`Operation`, `Date`) VALUES ('" . $_SESSION['thali'] . "','".$_SESSION['thaliid']."', 'Stop Thali','" . $today . "')") or die(mysqli_error($link));
+$time = new DateTime($cutoffTime);
+$time1 = date_format($time, 'H:i');
+$time = new DateTime($startTime);
+$time2 = date_format($time, 'H:i');
+
+$current = date("H:i");
+if ($current > $time1 && $current < $time2) {
+  $cutoffmessage =  'Stop thali not allowed post 6 PM. Please message 9503054797';
+  header("Location: index.php?status=$cutoffmessage");
+  exit;
+}
+
+$update = mysqli_query($link, "UPDATE thalilist set Active='0' WHERE id = '" . $_SESSION['thaliid'] . "'") or die(mysqli_error($link));
+$update = mysqli_query($link, "UPDATE thalilist set Thali_stop_date='" . $today . "' WHERE id = '" . $_SESSION['thaliid'] . "'") or die(mysqli_error($link));
+
+mysqli_query($link, "update change_table set processed = 1 where userid = '" . $_SESSION['thaliid'] . "' and `Operation` in ('Start Thali','Stop Thali','Start Transport','Stop Transport') and processed = 0") or die(mysqli_error($link));
+mysqli_query($link, "INSERT INTO change_table (`Thali`, `userid`,`Operation`, `Date`) VALUES ('" . $_SESSION['thali'] . "','" . $_SESSION['thaliid'] . "', 'Stop Thali','" . $today . "')") or die(mysqli_error($link));
 
 $status = 'Stop Thali Successful';
 header("Location: index.php?status=$status");
-?>
