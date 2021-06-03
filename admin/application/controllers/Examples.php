281c1796355543a13c00378efe967f68f9904e39
Yusuf Rampurawala <yusuf4u52@gmail.com>
2021-06-03 21:53:13 +0530

    remove thalisize from admin

--- admin/application/controllers/Examples.php
+++ admin/application/controllers/Examples.php
@@ -146,7 +146,7 @@ class Examples extends CI_Controller
 		$crud = new grocery_CRUD();
 		$crud->set_model('Event_join');
 		$crud->set_table('event_response');
-		$crud->columns('thalino', 'eventid', 'response', 'name', 'thalisize', 'wingflat', 'society', 'sector', 'subsector');
+		$crud->columns('thalino', 'eventid', 'response', 'name', 'wingflat', 'society', 'sector', 'subsector');
 		$crud->unset_edit();
 		$crud->unset_delete();
 		$output = $crud->render();
