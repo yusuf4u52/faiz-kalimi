cbf44911e533e12c888a54314c58ec59b59d4b32
tzabuawa <taha_zabuawala@bmc.com>
2021-06-09 13:24:20 +0530

    no size, no transporter

--- users/email2.php
+++ users/email2.php
@@ -36,11 +36,13 @@ mysqli_query($link, "update change_table set processed = 1 where id in (" . impl
 //----------------- Transporter wise count daily----------------------
 $hijridate = getHijriDate($tomorrow_date);
 $msgvar .= "\n<b>Transporter Count $hijridate </b>\n";
-$sql = mysqli_query($link, "SELECT Transporter,
+$sql = mysqli_query($link, "SELECT
+					(case when Transporter IS NULL then 'No Transport' else Transporter end) AS Transporter,
 					count(*) as tcount,
     				sum(case when thalisize = 'Small' then 1 else 0 end) AS smallcount,
     				sum(case when thalisize = 'Medium' then 1 else 0 end) AS mediumcount,
-					sum(case when thalisize = 'Large' then 1 else 0 end) AS largecount
+					sum(case when thalisize = 'Large' then 1 else 0 end) AS largecount,
+					sum(case when thalisize IS NULL then 1 else 0 end) AS nullcount
 					FROM `thalilist` WHERE Active = 1 group by Transporter");
 $tomorrow_date = date("Y-m-d", strtotime("+ 1 day"));
 $pivot = array();
@@ -50,7 +52,8 @@ while ($row = mysqli_fetch_assoc($sql)) {
 	$pivot["small"][$row['Transporter']] = $row['smallcount'];
 	$pivot["medium"][$row['Transporter']] = $row['mediumcount'];
 	$pivot["large"][$row['Transporter']] = $row['largecount'];
-	$pivot["total"][$row['Transporter']] = (int) $row['smallcount'] + (int) $row['mediumcount'] + (int) $row['largecount'];
+	$pivot["no size"][$row['Transporter']] = $row['nullcount'];
+	$pivot["total"][$row['Transporter']] = (int) $row['smallcount'] + (int) $row['mediumcount'] + (int) $row['largecount'] + (int) $row['nullcount'];
 	$insert_sql = "INSERT INTO transporter_daily_count (`date`, `name`,`small`,`medium`,`large`, `count`) VALUES ('" . $tomorrow_date . "','" . $row['Transporter'] . "', '" . $row['smallcount'] . "', '" . $row['mediumcount'] . "', '" . $row['largecount'] . "', '" . $row['tcount'] . "')";
 	mysqli_query($link, $insert_sql) or die(mysqli_error($link));
 }
@@ -61,13 +64,15 @@ $totalcountonsize = mysqli_query($link, "SELECT
 					count(*) as tcount,
     				sum(case when thalisize = 'Small' then 1 else 0 end) AS small,
     				sum(case when thalisize = 'Medium' then 1 else 0 end) AS medium,
-					sum(case when thalisize = 'Large' then 1 else 0 end) AS large
+					sum(case when thalisize = 'Large' then 1 else 0 end) AS large,
+					sum(case when thalisize IS NULL then 1 else 0 end) AS none
 					FROM `thalilist` WHERE Active = 1");
 
 $result = mysqli_fetch_row($totalcountonsize);
 $pivot["small"]["total"] = $result[1];
 $pivot["medium"]["total"] = $result[2];
 $pivot["large"]["total"] = $result[3];
+$pivot["no size"]["total"] = $result[4];
 $pivot["total"]["total"] = $result[0];
 
 mysqli_query($link, "INSERT INTO daily_thali_count (`Date`, `Hijridate`, `small`,`medium`,`large`, `Count`) VALUES ('" . $tomorrow_date . "','" . $hijridate . "','" . $result[1] . "','" . $result[2] . "','" . $$result[3] . "'," . $result[0] . ")") or die(mysqli_error($link));
