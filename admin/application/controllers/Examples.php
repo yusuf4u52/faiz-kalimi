d10d765902e8a811b2308f345ebc43d076d44c1e
Yusuf Rampurawala <yusuf4u52@gmail.com>
2021-06-18 15:54:07 +0530

    only show enabled event in response admin

--- admin/application/controllers/Examples.php
+++ admin/application/controllers/Examples.php
@@ -162,6 +162,7 @@ class Examples extends CI_Controller
 		$crud->set_model('Event_join');
 		$crud->set_table('event_response');
 		$crud->columns('thalino', 'eventid', 'response', 'name', 'CONTACT', 'wingflat', 'society','Full_Address', 'sector', 'subsector');
+		$crud->where('eventid in (select id from events WHERE enabled =1)');
 		$crud->unset_edit();
 		$crud->unset_delete();
 		$output = $crud->render();
