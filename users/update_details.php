3f6bf6c86c2590a6596c9ddd5839f4e0dd3223fc
Yusuf Rampurawala <yusuf4u52@gmail.com>
2021-06-24 08:46:46 +0530

    niyaz date commented

--- users/update_details.php
+++ users/update_details.php
@@ -150,12 +150,12 @@ if ($_POST) {
                     </div>
                   </div>
 
-                  <div class="form-group">
+                  <!-- <div class="form-group">
                     <label for="niyazdate" class="col-lg-2 control-label">Niyaz Date</label>
                     <div class="col-lg-10">
                       <input type="text" class="form-control" id="niyazdate" name="niyazdate" value='<?php echo $niyazdate; ?>' <?php echo !empty($niyazdate) ? "disabled" : ""; ?>>
                     </div>
-                  </div>
+                  </div> -->
                 <?php } ?>
 
                 <div class="form-group">
