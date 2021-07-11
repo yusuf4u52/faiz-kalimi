c6ac2b901d7ee2583df22c4c41a4ae9b98a7acf3
Yusuf Rampurawala <yusuf4u52@gmail.com>
2021-07-11 10:44:17 +0530

    fixing musaid details link not working issue

--- users/musaid.php
+++ users/musaid.php
@@ -25,9 +25,15 @@ $max_days = mysqli_fetch_row(mysqli_query($link, "SELECT MAX(thalicount) as max 
 $max_days_previous = mysqli_fetch_row(mysqli_query($link, "SELECT MAX(thalicount) as max FROM `$previous_thalilist`"));
 
 if (isset($_SESSION['role']) && $_SESSION['role'] === 'superadmin') {
-	$musaid_list = mysqli_query($link, "SELECT `id`,`email`,`username` FROM `users`");
+	$musaid_list = mysqli_fetch_all(mysqli_query($link, "SELECT `id`,`email`,`username` FROM `users` WHERE `role` in ('musaid','admin','superadmin')"), MYSQLI_ASSOC);
 } else {
-	$musaid_list = 	$musaid_list = mysqli_query($link, "SELECT `id`,`email`,`username` FROM `users` WHERE `email` = '" . $_SESSION['email'] . "'");
+	$musaid_list = array(
+		array(
+			'id' => 0,
+			'username' => $_SESSION['email'],
+			'email' => $_SESSION['email']
+		)
+	);
 }
 ?>
 <!DOCTYPE html>
@@ -49,9 +55,10 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === 'superadmin') {
 		<div class="row">
 			<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 				<?php
-				while ($musaid = mysqli_fetch_assoc($musaid_list)) {
+				foreach ($musaid_list as $musaid) {
 					$result = mysqli_query($link, "SELECT id, Thali, Active, Name, ITS_No, contact, yearly_hub, total_pending, Previous_Due, Paid, thalicount, WhatsApp FROM thalilist where Active in (0,1) AND musaid='" . $musaid['email'] . "'");
-					$musaid_thali_count = mysqli_num_rows($result);
+					$thali_details = mysqli_fetch_all($result, MYSQLI_ASSOC);
+					$musaid_thali_count = count($thali_details);
 					if ($musaid_thali_count > 0) {
 				?>
 						<div class="panel panel-default">
@@ -62,7 +69,7 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === 'superadmin') {
 									</a>
 								</h4>
 							</div>
-							<div id="collapse<?php echo $musaid['id']; ?>" class="panel-collapse collapse <?php if (mysqli_num_rows($musaid_list) == 1) echo "in"; ?>" role="tabpanel" aria-labelledby="heading<?php echo $musaid['id']; ?>">
+							<div id="collapse<?php echo $musaid['id']; ?>" class="panel-collapse collapse <?php if (count($musaid_list) == 1) echo "in"; ?>" role="tabpanel" aria-labelledby="heading<?php echo $musaid['id']; ?>">
 								<div class="panel-body">
 									<table class="table table-hover">
 										<thead>
@@ -80,14 +87,11 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === 'superadmin') {
 										</thead>
 										<tbody>
 											<?php
-											while ($values = mysqli_fetch_assoc($result)) {
-												$commit = mysqli_query($link, "SELECT concat(commit_date, ' / ', rs) as data FROM hub_commitment where rs !=0 and thali='" . $values['Thali'] . "'");
-												$all_dates = array();
-												while ($all_data = mysqli_fetch_assoc($commit)) {
-													$all_dates[] = $all_data['data'];
-												}
-
-												$comments = mysqli_query($link, "SELECT `hub_commitment`.`comments`, `hub_commitment`.`timestamp`, `thalilist`.`Email_ID` FROM hub_commitment INNER JOIN `thalilist` on `hub_commitment`.`author_id` = `thalilist`.`id` where comments is not null and `hub_commitment`.`thali`='" . $values['Thali'] . "' ORDER BY `timestamp` DESC");
+											foreach ($thali_details as $values) {
+												$commit = mysqli_query($link, "SELECT concat(commit_date, ' / ', rs) FROM hub_commitment where rs !=0 and thali='" . $values['Thali'] . "'");
+												$all_data = mysqli_fetch_all($commit);
+												$all_dates = array_column($all_data, 0);
+												$comments = mysqli_fetch_all(mysqli_query($link, "SELECT `hub_commitment`.`comments`, `hub_commitment`.`timestamp`, `thalilist`.`Email_ID` FROM hub_commitment INNER JOIN `thalilist` on `hub_commitment`.`author_id` = `thalilist`.`id` where comments is not null and `hub_commitment`.`thali`='" . $values['Thali'] . "' ORDER BY `timestamp` DESC"), MYSQLI_ASSOC);
 											?>
 												<form method="post">
 													<tr>
@@ -100,13 +104,12 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === 'superadmin') {
 															</a>
 														</td>
 														<td>
-															<a target="_blank" href="https://wa.me/91<?php echo $values['WhatsApp']; ?>?text=Salaam Bhai">WhatsApp</a> |
-															<?php
-															if ($values['Active'] == '1') { ?>
+															<a target="_blank" href="https://wa.me/91<?php echo $values['WhatsApp']; ?>?text=Salaam Bhai">WhatsApp</a>
+															<!-- <?php if ($values['Active'] == '1') { ?>
 																<a href="#" data-key="startstopthaali" data-thali="<?php echo $values['Thali']; ?>" data-active="0">Stop Thaali</a>
 															<?php } else { ?>
 																<a href="#" data-key="startstopthaali" data-thali="<?php echo $values['Thali']; ?>" data-active="1">Start Thaali</a>
-															<?php } ?>
+															<?php } ?> -->
 														</td>
 														<td><?php echo $values['Active'] ? '<span class="label label-success">Active</span>' : '<span class="label label-danger">Inactive</span>'; ?></td>
 														<td><?php echo $values['Name']; ?></td>
@@ -115,7 +118,7 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === 'superadmin') {
 														<td><?php echo "<pre>" . implode(",\n", $all_dates) . "</pre>"; ?><input type="text" name="date" class="datepicker" autocomplete="off"><input type="number" name="rs"></td>
 														<td>
 															<?php
-															while ($comment = mysqli_fetch_assoc($comments)) {
+															foreach ($comments as $comment) {
 															?>
 																<?php echo  $comment['comments']; ?><br>
 																<span style="color: grey">- <?php echo explode('@', $comment['Email_ID'])[0]; ?> <?php echo date('d/m/Y', strtotime($comment['timestamp'])); ?></span>
@@ -133,7 +136,7 @@ if (isset($_SESSION['role']) && $_SESSION['role'] === 'superadmin') {
 										</tbody>
 									</table>
 									<?php
-									while ($values = mysqli_fetch_assoc($result)) {
+									foreach ($thali_details as $values) {
 										include('_thali_details_musaid.php');
 									}
 									?>
