11a3580e338b8000f939e48e19a362b23861c69e
Yusuf Rampurawala <yusuf4u52@gmail.com>
2021-12-23 16:36:47 +0530

    Update changes should work even if thali is not active

--- users/update_details.php
+++ users/update_details.php
@@ -16,7 +16,7 @@ if ($_POST) {
                                       WhatsApp='" . $_POST["whatsapp"] . "'
                                       WHERE Email_id = '" . $_SESSION['email'] . "'") or die(mysqli_error($link));
 
-  if ($_POST['address'] != $_SESSION['old_address'] && $_SESSION['active'] == 1) {
+  if ($_POST['address'] != $_SESSION['old_address']) {
     mysqli_query($link, "UPDATE thalilist set Transporter=NULL, sector = NULL, subsector = NULL where id ='" . $_SESSION['thaliid'] . "'");
     mysqli_query($link, "update change_table set processed = 1 where userid = '" . $_SESSION['thaliid'] . "' and `Operation` in ('Update Address') and processed = 0") or die(mysqli_error($link));
     mysqli_query($link, "INSERT INTO change_table (`Thali`,`userid`, `Operation`, `Date`) VALUES ('" . $_SESSION['thali'] . "','" . $_SESSION['thaliid'] . "', 'Update Address','" . $today . "')") or die(mysqli_error($link));
