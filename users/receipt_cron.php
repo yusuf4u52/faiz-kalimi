08cf874d7c6ef02dfd6aa075dfb64908c38e5880
Yusuf Rampurawala <yusuf4u52@gmail.com>
2022-08-25 23:14:26 +0530

    fix algo

--- users/receipt_cron.php
+++ users/receipt_cron.php
@@ -89,45 +89,43 @@ $counter = 1;
 $file = fopen("fmbanualniyaz.csv", "r");
 while (($column = fgetcsv($file)) !== FALSE) {
     if ($column[13] != "Cancelled" && $column[17] == "Faizul Mawaidil Burhaniyah") {
-        if ($counter != 1) {
-            $receiptno = "";
-            if (isset($column[0])) {
-                $receiptno = mysqli_real_escape_string($link, $column[0]);
-            }
-            $thalino = "";
-            if (isset($column[1])) {
-                $thalino = mysqli_real_escape_string($link, $column[1]);
-            }
-            $amount = "";
-            if (isset($column[4])) {
-                $amount = mysqli_real_escape_string($link, $column[4]);
-                $amount = str_replace('₹', '', $amount);
-                $amount = str_replace(',', '', $amount);
-                $amount = intval($amount);
-            }
-            $date = "";
-            if (isset($column[3])) {
-                $datefromcsv = mysqli_real_escape_string($link, $column[3]);
-                $datestring = getHijriDate($datefromcsv);
-            } else {
-                echo "Date cannot be empty for " . $receiptno;
-            }
-
-            $receivedby = "";
-            if (isset($column[10])) {
-                $receivedby = mysqli_real_escape_string($link, $column[10]);
-            }
-
-            $sql = "select NAME,id from thalilist WHERE thali = '$thalino'";
-            $result = mysqli_query($link, $sql) or die(mysqli_error($link));
-            $name = mysqli_fetch_assoc($result);
-
-            $sqlInsert = "insert into niyaz (`Receipt_No`,`Thali_No`,`userid`, `name`, `Date`,`Amount`,`received_by`)
-                   values ('$receiptno', '$thalino', '" . $name['id'] . "', '" . $name['NAME'] . "', '$datestring', '$amount', '$receivedby')";
-            mysqli_query($link, $sqlInsert) or die(mysqli_error($link));
+        $receiptno = "";
+        if (isset($column[0])) {
+            $receiptno = mysqli_real_escape_string($link, $column[0]);
         }
-        $counter++;
+        $thalino = "";
+        if (isset($column[1])) {
+            $thalino = mysqli_real_escape_string($link, $column[1]);
+        }
+        $amount = "";
+        if (isset($column[4])) {
+            $amount = mysqli_real_escape_string($link, $column[4]);
+            $amount = str_replace('₹', '', $amount);
+            $amount = str_replace(',', '', $amount);
+            $amount = intval($amount);
+        }
+        $date = "";
+        if (isset($column[3])) {
+            $datefromcsv = mysqli_real_escape_string($link, $column[3]);
+            $datestring = getHijriDate($datefromcsv);
+        } else {
+            echo "Date cannot be empty for " . $receiptno;
+        }
+
+        $receivedby = "";
+        if (isset($column[10])) {
+            $receivedby = mysqli_real_escape_string($link, $column[10]);
+        }
+
+        $sql = "select NAME,id from thalilist WHERE thali = '$thalino'";
+        $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+        $name = mysqli_fetch_assoc($result);
+
+        $sqlInsert = "insert into niyaz (`Receipt_No`,`Thali_No`,`userid`, `name`, `Date`,`Amount`,`received_by`)
+                   values ('$receiptno', '$thalino', '" . $name['id'] . "', '" . $name['NAME'] . "', '$datestring', '$amount', '$receivedby')";
+        mysqli_query($link, $sqlInsert) or die(mysqli_error($link));
     }
+    $counter++;
 }
 
 // update sherullah niyaz
@@ -136,45 +134,43 @@ $counter = 1;
 $file = fopen("fmbsherullah.csv", "r");
 while (($column = fgetcsv($file)) !== FALSE) {
     if ($column[13] != "Cancelled" && $column[17] == "Faizul Mawaidil Burhaniyah") {
-        if ($counter != 1) {
-            $receiptno = "";
-            if (isset($column[0])) {
-                $receiptno = mysqli_real_escape_string($link, $column[0]);
-            }
-            $thalino = "";
-            if (isset($column[1])) {
-                $thalino = mysqli_real_escape_string($link, $column[1]);
-            }
-            $amount = "";
-            if (isset($column[4])) {
-                $amount = mysqli_real_escape_string($link, $column[4]);
-                $amount = str_replace('₹', '', $amount);
-                $amount = str_replace(',', '', $amount);
-                $amount = intval($amount);
-            }
-            $date = "";
-            if (isset($column[3])) {
-                $datefromcsv = mysqli_real_escape_string($link, $column[3]);
-                $datestring = getHijriDate($datefromcsv);
-            } else {
-                echo "Date cannot be empty for " . $receiptno;
-            }
-
-            $receivedby = "";
-            if (isset($column[10])) {
-                $receivedby = mysqli_real_escape_string($link, $column[10]);
-            }
-
-            $sql = "select NAME,id from thalilist WHERE thali = '$thalino'";
-            $result = mysqli_query($link, $sql) or die(mysqli_error($link));
-            $name = mysqli_fetch_assoc($result);
-
-            $sqlInsert = "insert into sherullah (`Receipt_No`,`Thali_No`,`userid`, `name`, `Date`,`Amount`,`received_by`)
-                   values ('$receiptno', '$thalino', '" . $name['id'] . "', '" . $name['NAME'] . "', '$datestring', '$amount', '$receivedby')";
-            mysqli_query($link, $sqlInsert) or die(mysqli_error($link));
+        $receiptno = "";
+        if (isset($column[0])) {
+            $receiptno = mysqli_real_escape_string($link, $column[0]);
         }
-        $counter++;
+        $thalino = "";
+        if (isset($column[1])) {
+            $thalino = mysqli_real_escape_string($link, $column[1]);
+        }
+        $amount = "";
+        if (isset($column[4])) {
+            $amount = mysqli_real_escape_string($link, $column[4]);
+            $amount = str_replace('₹', '', $amount);
+            $amount = str_replace(',', '', $amount);
+            $amount = intval($amount);
+        }
+        $date = "";
+        if (isset($column[3])) {
+            $datefromcsv = mysqli_real_escape_string($link, $column[3]);
+            $datestring = getHijriDate($datefromcsv);
+        } else {
+            echo "Date cannot be empty for " . $receiptno;
+        }
+
+        $receivedby = "";
+        if (isset($column[10])) {
+            $receivedby = mysqli_real_escape_string($link, $column[10]);
+        }
+
+        $sql = "select NAME,id from thalilist WHERE thali = '$thalino'";
+        $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+        $name = mysqli_fetch_assoc($result);
+
+        $sqlInsert = "insert into sherullah (`Receipt_No`,`Thali_No`,`userid`, `name`, `Date`,`Amount`,`received_by`)
+                   values ('$receiptno', '$thalino', '" . $name['id'] . "', '" . $name['NAME'] . "', '$datestring', '$amount', '$receivedby')";
+        mysqli_query($link, $sqlInsert) or die(mysqli_error($link));
     }
+    $counter++;
 }
 
 // update zabihat
