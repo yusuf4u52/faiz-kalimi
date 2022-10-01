20263d3f380c4ef5accf36e06894afd038fa56b5
Yusuf Rampurawala <yusuf4u52@gmail.com>
2022-10-01 15:32:30 +0530

    Changing instance key

--- sms/_send_sms_xml.php
+++ sms/_send_sms_xml.php
@@ -38,7 +38,7 @@ function send_sms_to_records($conn, $message)
 		$sms_body_encoded = rawurlencode($message_formatted);
 		// send sms
 		// $sendurl = "https://www.fast2sms.com/dev/bulkV2?authorization=$smsauthkey&route=v3&sender_id=TXTIND&message=$sms_body_encoded&language=english&flash=0&numbers=$number";
-		$sendurl = "https://senderomatic.xyz/api/send-media.php?number=91$number&msg=$sms_body_encoded&media=https://".$_SERVER['HTTP_HOST']."/fmb/users/images/fmb-hdfc-qr.jpeg&apikey=$apikey&instance=159q8h7HCez6RAN";
+		$sendurl = "https://senderomatic.xyz/api/send-media.php?number=91$number&msg=$sms_body_encoded&media=https://".$_SERVER['HTTP_HOST']."/fmb/users/images/fmb-hdfc-qr.jpeg&apikey=$apikey&instance=G872gO9M5km6xef";
 		file_get_contents($sendurl);
 	}
 	return "success";
