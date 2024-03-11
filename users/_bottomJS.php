e98d431ae6c1385aa5b8c6fcb602939f112cb80e
moizsahil5352 <154581072+moizsahil5352@users.noreply.github.com>
2024-03-11 16:03:39 +0530

    Update _bottomJS.php
    Add Calendar js for calendar view

--- users/_bottomJS.php
+++ users/_bottomJS.php
@@ -5,5 +5,7 @@
 <script src="javascript/moment-hijri.js?v=1"></script>
 <script src="javascript/hijriDate.js?v=1"></script>
 <script src="javascript/index.js?v=1"></script>
-<script src="src/custom.js?v=1"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+// Add Calendar js for calendar view
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.5/main.min.js"></script>
+<script src="src/custom.js?v=1"></script>
