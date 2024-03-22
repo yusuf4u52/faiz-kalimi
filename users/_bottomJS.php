90c24067b9962a83c8cb06dad0e95781306c1633
moizsahil5352 <154581072+moizsahil5352@users.noreply.github.com>
2024-03-22 12:26:57 +0530

    Update _bottomJS.php
    Add Calendar & Custom js

--- users/_bottomJS.php
+++ users/_bottomJS.php
@@ -4,6 +4,7 @@
 <script src="javascript/moment-2.11.1-min.js?v=1"></script>
 <script src="javascript/moment-hijri.js?v=1"></script>
 <script src="javascript/hijriDate.js?v=1"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.5/main.min.js"></script>
 <script src="javascript/index.js?v=1"></script>
 <script src="src/custom.js?v=1"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
