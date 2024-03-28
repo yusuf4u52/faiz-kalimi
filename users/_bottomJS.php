b682444e94dac2c45d5c63a61825e76b941a811c
moizsahil5352 <154581072+moizsahil5352@users.noreply.github.com>
2024-03-28 17:30:01 +0530

    Update _bottomJS.php
    Update jquery

--- users/_bottomJS.php
+++ users/_bottomJS.php
@@ -1,4 +1,4 @@
-<script src="javascript/jquery-2.2.0.min.js?v=1"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="javascript/jquery.dynatable.js?v=1"></script>
 <script src="javascript/bootstrap-3.3.6.min.js?v=1"></script>
 <script src="javascript/moment-2.11.1-min.js?v=1"></script>
