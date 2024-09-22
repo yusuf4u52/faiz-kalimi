6c9a7bbdd5645192a4713cb38f5e33c2d54ab580
MoizSahil <moizlife@gmail.com>
2024-09-22 11:58:49 +0530

    Add 1.5 quantity for rice count and update the pwa as well.

--- styles/js/register-sw.js
+++ styles/js/register-sw.js
@@ -73,67 +73,67 @@ if ('serviceWorker' in navigator) {
          */
         function injectSnackbarCss() {
             const css = `body.snackbar--show .snackbar {
-              -webkit-transform: translateY(0);
-              transform: translateY(0); 
-          }
-          .snackbar {
-              box-sizing: border-box;
-              background-color: #121213;
-              color: #fff;
-              padding: 10px 55px 10px 10px;
-              position: fixed;
-              z-index: 9999999999999999;
-              left: 15px;
-              bottom: 15px;
-              border-radius: 5px 8px 8px 5px;
-              max-width: 90%;
-              min-height: 48px;
-              line-height: 28px;
-              font-size: 16px;
-              -webkit-transform: translateY(150%);
-              transform: translateY(150%);
-              will-change: transform;
-              -webkit-transition: -webkit-transform 200ms ease-in-out;
-              -webkit-transition-delay: 0s;
-                      transition-delay: 0s;
-              -webkit-transition: -webkit-transform 200ms ease-in-out false;
-              transition: -webkit-transform 200ms ease-in-out false;
-              transition: transform 200ms ease-in-out false;
-              transition: transform 200ms ease-in-out false, -webkit-transform 200ms ease-in-out false; 
-          }
-          .snackbar p {
-              margin: 0;
-              color: #fff;
-              text-align: center; 
-          }
-          .snackbar .snackbar-close {
-              position: absolute;
-              top: 0;
-              right: 0;
-              width: 45px;
-              height: 100%;
-              padding: 0;
-              background: #2a2a2a;
-              border: none;
-              font-size: 28px;
-              font-weight: normal;
-              border-radius: 0 5px 5px 0;
-              color: #FFF;
-              font-family: Arial, Helvetica, sans-serif;
-          }
-          .snackbar .snackbar-close:hover,
-          .snackbar .snackbar-close:focus {
-              background: #3f3f3f;
-          }
-          .snackbar a {
-              color: #FFF;
-              font-weight: bold;
-              text-decoration: underline; 
-          }`;
-
+                -webkit-transform: translateY(0);
+                transform: translateY(0); 
+            }
+            .snackbar {
+                box-sizing: border-box;
+                background-color: #121213;
+                color: #fff;
+                padding: 10px 55px 10px 10px;
+                position: fixed;
+                z-index: 9999999999999999;
+                left: 15px;
+                bottom: 15px;
+                border-radius: 5px 8px 8px 5px;
+                max-width: 90%;
+                min-height: 48px;
+                line-height: 28px;
+                font-size: 16px;
+                -webkit-transform: translateY(150%);
+                transform: translateY(150%);
+                will-change: transform;
+                -webkit-transition: -webkit-transform 200ms ease-in-out;
+                -webkit-transition-delay: 0s;
+                        transition-delay: 0s;
+                -webkit-transition: -webkit-transform 200ms ease-in-out false;
+                transition: -webkit-transform 200ms ease-in-out false;
+                transition: transform 200ms ease-in-out false;
+                transition: transform 200ms ease-in-out false, -webkit-transform 200ms ease-in-out false; 
+            }
+            .snackbar p {
+                margin: 0;
+                color: #fff;
+                text-align: center; 
+            }
+            .snackbar .snackbar-close {
+                position: absolute;
+                top: 0;
+                right: 0;
+                width: 45px;
+                height: 100%;
+                padding: 0;
+                background: #2a2a2a;
+                border: none;
+                font-size: 28px;
+                font-weight: normal;
+                border-radius: 0 5px 5px 0;
+                color: #FFF;
+                font-family: Raleway, Helvetica, Arial, sans-serif;
+            }
+            .snackbar .snackbar-close:hover,
+            .snackbar .snackbar-close:focus {
+                background: #3f3f3f;
+            }
+            .snackbar a {
+                color: #FFF;
+                font-weight: bold;
+                text-decoration: underline; 
+            }`;
+                
             const head = document.head || document.getElementsByTagName('head')[0];
             const style = document.createElement('style');
-
+    
             style.type = 'text/css';
             if (style.styleSheet) {
                 // This is required for IE8 and below.
@@ -141,9 +141,9 @@ if ('serviceWorker' in navigator) {
             } else {
                 style.appendChild(document.createTextNode(css));
             }
-
+    
             head.appendChild(style);
-        }
+        }    
 
         /**
          * Show the state of the mode of connectivity to the user :  back onLine | go offLine
@@ -191,7 +191,7 @@ if ('serviceWorker' in navigator) {
                     deferredPrompt.preventDefault();
                 }
 
-                var a2hsBanner = document.getElementsByClassName("superpwa-sticky-banner");
+                var a2hsBanner = document.getElementsByClassName("kalimi-sticky-banner");
                 if (a2hsBanner.length) {
                     deferredPrompt.preventDefault();
                     //Disable on desktop
@@ -207,7 +207,7 @@ if ('serviceWorker' in navigator) {
         })
 
         window.addEventListener('appinstalled', function (evt) {
-            var a2hsBanner = document.getElementsByClassName("superpwa-sticky-banner");
+            var a2hsBanner = document.getElementsByClassName("kalimi-sticky-banner");
             if (a2hsBanner.length) {
                 for (var i = 0; i < a2hsBanner.length; i++) {
                     var showbanner = a2hsBanner[i].getAttribute("data-show");
@@ -217,7 +217,7 @@ if ('serviceWorker' in navigator) {
             }
         });
 
-        var a2hsviaClass = document.getElementsByClassName("superpwa-add-via-class");
+        var a2hsviaClass = document.getElementsByClassName("kamlimi-add-via-class");
         if (a2hsviaClass !== null) {
             for (var i = 0; i < a2hsviaClass.length; i++) {
                 a2hsviaClass[i].addEventListener("click", addToHome);
@@ -229,7 +229,7 @@ if ('serviceWorker' in navigator) {
             deferredPrompt.prompt();
             deferredPrompt.userChoice.then(function (choiceResult) {
                 if (choiceResult.outcome === "accepted") {
-                    var a2hsBanner = document.getElementsByClassName("superpwa-sticky-banner");
+                    var a2hsBanner = document.getElementsByClassName("kalimi-sticky-banner");
                     if (a2hsBanner) {
                         for (var i = 0; i < a2hsBanner.length; i++) {
                             var showbanner = a2hsBanner[i].getAttribute("data-show");
