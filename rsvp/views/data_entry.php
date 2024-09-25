baf9052c006a3e2421d10cd80f34e7ab5c8471af
hatimkamaal <hatim.kamaal@gmail.com>
2024-09-25 18:14:03 +0530

    HOF ID validation added to 8 digit

--- rsvp/views/data_entry.php
+++ rsvp/views/data_entry.php
@@ -16,7 +16,7 @@ function content_display()
     <input type="hidden" value="<?= $miqaat_id ?>" name="miqaat_id" id="miqaat_id">
     <div class="input-group mb-3">
       <input type="text" required class="form-control" name="hof_id" id="hof_id" placeholder="HOF ID"
-        aria-label="Sabeel number" aria-describedby="button-addon2">
+        pattern="^[0-9]{8}$" aria-label="HOF ID" aria-describedby="button-addon2">
       <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
     </div>
   </form>
