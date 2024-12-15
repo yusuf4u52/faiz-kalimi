ecb4a3801c0cf02fb976d15f92e029a551252cee
yusuf4u52 <yusuf4u52@gmail.com>
2024-12-15 18:10:49 +0530

    Revert "Some Pages with only design changes"

--- fmb/users/hoobHistory.php
+++ fmb/users/hoobHistory.php
@@ -1,43 +1,41 @@
 <?php
 error_reporting(0);
-include('header.php');
-include('navbar.php');
+include('_authCheck.php');
 ?>
-<div class="content mt-5">
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <div class="card">
-          <div class="card-body">
-            <table class="table table-striped display" width="100%">
-              <thead>
-                <tr>
-                  <th>Receipt No</th>
-                  <th>Amount</th>
-                  <th>Date</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
-                $query = "SELECT r.* FROM receipts r, thalilist t WHERE r.userid = t.id and t.Email_ID ='" . $_SESSION['email'] . "' ORDER BY Date ASC";
-                $result = mysqli_query($link, $query);
-                while ($row = mysqli_fetch_assoc($result)) {
-                  foreach ($row as $key => $value) {
-                    $row[$key] = stripslashes($value);
-                  }
-                  echo "<tr>";
-                  echo "<td>" . nl2br($row['Receipt_No']) . "</td>";
-                  echo "<td>" . nl2br($row['Amount']) . "</td>";
-                  echo "<td class=\"hijridate\">" . nl2br($row['Date']) . "</td>";
-                  echo "</tr>";
-                }
-                ?>
-              </tbody>
-            </table>
-          </div>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php include('_head.php'); ?>
+  </head>
+  <body>
+    <?php include('_nav.php'); ?>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <table class="table table-striped table-hover">
+          <tr>
+          <td><b>Receipt No</b></td>
+          <td><b>Amount</b></td>
+          <td><b>Date</b></td>
+          </tr>
+          <?php
+         
+          $query = "SELECT r.* FROM receipts r, thalilist t WHERE r.userid = t.id and t.Email_ID ='".$_SESSION['email']."' ORDER BY Date ASC";
+          $result = mysqli_query($link,$query);
+          while($row = mysqli_fetch_assoc($result)){ 
+          foreach($row AS $key => $value) { $row[$key] = stripslashes($value); } 
+            echo "<tr>";  
+            echo "<td>" . nl2br( $row['Receipt_No']) . "</td>";  
+            echo "<td>" . nl2br( $row['Amount']) . "</td>";  
+            echo "<td class=\"hijridate\">" . nl2br( $row['Date']) . "</td>";  
+            echo "</tr>"; 
+          } 
+          ?>
+          </table>
         </div>
       </div>
     </div>
-  </div>
-</div>
-<?php include('footer.php'); ?>
+    <?php include('_bottomJS.php'); ?>
+  </body>
+</html>
+
