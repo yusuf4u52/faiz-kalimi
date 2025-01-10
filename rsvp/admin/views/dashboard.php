c17ad7791dd098366feb8bd71f33c1f96a9d82b7
hatimkamaal <hatim.kamaal@gmail.com>
2025-01-10 10:45:52 +0530

    Update dashboard.php
    Extra DIV tag is removed

--- rsvp/admin/views/dashboard.php
+++ rsvp/admin/views/dashboard.php
@@ -14,8 +14,6 @@ function content_display()
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
         <div class="col-md-3 col-sm-6 col-12">
             <div class="card">
                 <div class="card-body">
