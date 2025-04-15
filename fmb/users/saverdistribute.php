90530190795e59d54ddf99abb7b670c3c5eb0b3c
moizsahil5352 <moizlife@gmail.com>
2025-04-15 19:19:07 +0530

    Modify & do changes on roti module

--- fmb/users/saverdistribute.php
+++ fmb/users/saverdistribute.php
@@ -3,7 +3,7 @@ include('connection.php');
 include('_authCheck.php');
 
 if( isset($_POST['action']) && $_POST['action'] == 'add_rdistribute' ) {
-    $recieved = mysqli_query($link, "SELECT * FROM fmb_roti_recieved WHERE `recieved_date` <= '".$_POST['distribution_date']."' order by `recieved_date` DESC LIMIT 1") or die(mysqli_error($link));
+    $recieved = mysqli_query($link, "SELECT * FROM fmb_roti_recieved WHERE `recieved_date` <= '".$_POST['distribution_date']."' AND `maker_id` = '" . $_POST['maker_id'] . "' order by `recieved_date` DESC LIMIT 1") or die(mysqli_error($link));
     if ($recieved->num_rows > 0) {
         $row_recieved = $recieved->fetch_assoc();
         $flour_left = $row_recieved['flour_left'];
@@ -18,7 +18,7 @@ if( isset($_POST['action']) && $_POST['action'] == 'add_rdistribute' ) {
 }
 
 if( isset($_POST['action']) && $_POST['action'] == 'edit_rdistribute' ) {
-    $recieved = mysqli_query($link, "SELECT * FROM fmb_roti_recieved WHERE `recieved_date` <= '".$_POST['distribution_date']."' order by `recieved_date` DESC LIMIT 1") or die(mysqli_error($link));
+    $recieved = mysqli_query($link, "SELECT * FROM fmb_roti_recieved WHERE `recieved_date` <= '".$_POST['distribution_date']."' AND `maker_id` = '" . $_POST['maker_id'] . "' order by `recieved_date` DESC LIMIT 1") or die(mysqli_error($link));
     if ($recieved->num_rows > 0) {
         $row_recieved = $recieved->fetch_assoc();
         $flour_left = $row_recieved['flour_left'];
