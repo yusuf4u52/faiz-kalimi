e7e629b0483f648b8e62f3c6022beb1857a101dc
hatimkamaal <hatim.kamaal@gmail.com>
2025-09-01 19:41:43 +0530

    Admin dashboard page bug fixes (#207)

--- rsvp/admin/views/its_upload.php
+++ rsvp/admin/views/its_upload.php
@@ -4,20 +4,37 @@ if_not_post_redirect('/dashboard');
 do_for_post('_manage_upload');
 
 function content_display() {
-    //$data = getAppData('display_data');
+    $uri = getAppData('BASE_URI');
     ?>
+    <form action="<?=$uri?>/its_upload" method="post">
     <div class="card">
         <div class="card-header row">
             <div class="col-6">Data Upload Result</div>
             <div class="card-body">
                 <?=getAppData('page_data')?>
-            </div>
+                <input type="hidden" name="action" value="dump"/>
+                <h2>Click UPLOAD, if you are sure that data is correct.</h2>
+                <button class="btn btn-outline-primary" type="submit" id="button-addon2">UPLOAD</button>
+                <a href="<?=$uri?>/dashboard" class="btn btn-outline-danger">CANCEL</a>
+            </div>            
         </div>
     </div>
+    </form>
     <?php
 }
 
 function _manage_upload() {
+    $action = $_POST['action'] ?? '';
+    if( $action === 'dump') {
+         $query = 'DELETE FROM ITS_DB; INSERT INTO ITS_DB SELECT * FROM ITS_DB_TRANSIT;';
+         $resp = execute_query($query, true, true);
+         do_redirect_with_message('/dashboard', 'Data has been loaded successfully.');
+    } else {
+        _manage_upload_1();
+    }
+}
+
+function _manage_upload_1() {
     $home_link = '/dashboard';
     $message = '';
     $target_file = null;
@@ -61,47 +78,14 @@ function _manage_upload() {
                 foreach ($xlsx->rows() as $row) {
                     if ($first) {
                         $first = false;
-                        //$page_data .= '<tr><th>Error</th><th>' . implode('</th><th>', $row) . '</th></tr>';
-    
-                        // add_to_transit($row[0],$row[1],$row[2],$row[3],$row[4]
-                        // ,$row[7],$row[9],$row[5],$row[10],$row[11]);
-
-                        // $query = 'INSERT INTO ITS_DB_TRANSIT (' . implode(',', $row) . ')
-                        //         VALUES (' . str_repeat("?,", count($row) - 1) . '?)';
-                        // array_shift($row);
-                        // $query .= ' ON DUPLICATE KEY UPDATE
-                        //         ' . implode('=?,', $row) . '=?;';
+                        $page_data .= '<tr><th>Status</th><th>' . implode('</th><th>', $row) . '</th></tr>';
                     } else {
                         // if 3rd colum is blank, means Mumin has taken transfer out but not transfer in elsewhere.
                         if (strlen($row[2]) == 0) {
-                            //$page_data .= '<tr><td>IGNORED : ' . implode('</td><td>', $row) . '</td></tr>';
+                            $page_data .= '<tr><td><b>IGNORED</b>' . implode('</td><td>', $row) . '</td></tr>';
                             continue;
-                        }
+                        } 
     
-                        // $row2 = $row;
-                        // array_shift($row);
-    
-                        // $finalData = array_merge($row2, $row);
-                        
-                        /**
-                         * A- ITS_ID
-                         * B-HOF_ID
-                         * C-TanzeemFile_No
-                         * D-Full_Name
-                         * E-Age
-                         * F-Gender
-                         * G-Misaq
-                         * H,I,j,K
-                         * L-Address
-                         * M,N,O,P
-                         * Q-Sector
-                         * R-Sub_Sector
-                         * 
-                         */
-                        // $rslt = add_to_transit($row[0],$row[1],$row[2],$row[3]
-                        // ,$row[4],$row[7],$row[9],$row[5],$row[10],$row[11]);
-                        // $rslt = add_to_transit($row[0],$row[1],$row[2],$row[3]
-                        // ,$row[4],$row[6],$row[11],$row[5],$row[14],$row[15]);
                         $itsid = $row[0];
                         $hofid = $row[1];
                         $sabeel = $row[2];
@@ -113,7 +97,6 @@ function _manage_upload() {
                         $sector = $row[8];
                         $subsector = $row[9];
                         $mohalla = 'Kalimi';
-
                         $query = "INSERT INTO its_data_transit (its_id,hof_id,sabeel_no,full_name,age,
                         misaq,address,gender,sector,subsector,mohallah) 
                 VALUES ('$itsid','$hofid','$sabeel','$name','$age','$misaq','$address','$gender','$sector','$subsector','$mohalla') 
@@ -123,29 +106,17 @@ function _manage_upload() {
 
                         $resp = execute_query($query, true);
                         if( $resp->count > 0) {
-                            $page_data .= "<tr><td>DONE : $itsid - $name</td></tr>";
+                            $page_data .= '<tr><td><b>SUCCESS</b>' . implode('</td><td>', $row) . '</td></tr>';
                         } else {
-                            $page_data .= "<tr><td>NO CHANGE : $itsid - $name</td></tr>";
+                            $page_data .= '<tr><td><b>NO CHANGE</b>' . implode('</td><td>', $row) . '</td></tr>';
                         }                        
                     }
                 }
                 $page_data .= '</table>';
-    
-                // if( $error_found == 0 ) {
-                //     //move_from_transit_to_its_data();
-                    $page_data .= '<h2>ITS Data Updated Successfully</h2>';
-                // } else {
-                //     $page_data .= '<h2>ITS Data Updated with Errors</h2>';
-                // }
 
-                    $query = 'DELETE FROM ITS_DB; INSERT INTO ITS_DB SELECT * FROM ITS_DB_TRANSIT;';
-                    $resp = execute_query($query, true, true);
-
-                    setAppData('page_data', $page_data);
-                // }
+                setAppData('page_data', $page_data);
             } else {
                 do_redirect_with_message($home_link, SimpleXLSX::parseError());
-                //redirectWithMessage('dashboard', SimpleXLSX::parseError());
             }
         } else {
             do_redirect_with_message($home_link, "Sorry, there was an error uploading your file.");
