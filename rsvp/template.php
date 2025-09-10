8edc3344bc6f6802ced47c88b2c6ada78e071832
moizsahil5352 <moizlife@gmail.com>
2025-09-10 16:01:42 +0530

    RSVP new design

--- rsvp/template.php
+++ rsvp/template.php
@@ -21,28 +21,34 @@ if (is_record_found($result)) {
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="/rsvp/assets/imgs/Logo.png" />
-    <link href="https://fonts.googleapis.com/css?family=Lato:200,300,400,500,500i,600,700,800,900" rel="stylesheet">
+    <link rel="shortcut icon" href="/rsvp/assets/imgs/logo.png" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alice&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css" />
     <link rel="stylesheet" href="/fmb/users/assets/css/main.css" />
 </head>
 <body>
-    <header class="rsvp-header">
-        <nav class="navbar">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="<?= getAppData('BASE_URI') ?>"><img class="img-fluid" src="/rsvp/assets/imgs/Logo.png" alt="Miqaat RSVP (Kalimi Mohalla - Poona)" width="72" height="72" /></a>
-                <a class="navbar-brand text-end" href="<?= getAppData('BASE_URI') ?>">Miqaat RSVP <br/> (Kalimi Mohalla - Poona)</a>
+    <header class="header">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-6">
+                    <a href="<?= getAppData('BASE_URI') ?>"><img class="img-fluid" src="/rsvp/assets/imgs/logo.png" alt="Miqaat RSVP (Kalimi Mohalla - Poona)" width="153" height="153" /></a>
+                </div>
+                <div class="col-6 text-end">
+                    <h5><a href="<?= getAppData('BASE_URI') ?>">Miqaat RSVP <br/> (Kalimi Mohalla - Poona)</a></h5>
+                </div>
             </div>
-        </nav>
+        </div>
     </header>
-    <div class="rsvp-body mt-5">
-        <div class="container">
+    <div class="content mt-4">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
-                             <h2 clas="mb-3">Miqaat : <?= $miqaat_name ?></h2>
+                             <h2 clas="mb-3">Miqaat: <?= $miqaat_name ?></h2>
                             <?php
                             if (isset($end_datetime)) {
                                 $miqaat_ends = date_format($end_datetime, 'd/m/Y H:i:s');
@@ -69,7 +75,7 @@ if (is_record_found($result)) {
         </div>
     </div>
     <footer class="rsvp-footer text-center my-2">
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
                     <p class="mb-1"><small><strong>Helpline:</strong> <a href="https://wa.me/+917767825353">+91 77678 25353</a></small></p>
