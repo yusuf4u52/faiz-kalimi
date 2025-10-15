7b1724511196fac77e1c67e7123b5baca41931ea
moizsahil5352 <moizlife@gmail.com>
2025-10-15 16:50:03 +0530

    month wise report

--- fmb/users/transporter/report.php
+++ fmb/users/transporter/report.php
@@ -7,11 +7,8 @@ include('../navbar.php');
     <div class="card-body">
         <div class="row">
             <div class="col-12">
-                <?php if (isset($_GET['payment_week'])) { 
-                    list($year, $week) = explode('-W', $_GET['payment_week']);
-                    $start_date = date('Y-m-d', strtotime($year . 'W' . $week));
-                    $end_date = date('Y-m-d', strtotime($year . 'W' . $week . '+6 days')); ?>
-                    <h2 class="mb-3">Transporter Report from <?php echo $start_date; ?> to <?php echo $end_date; ?></h2>
+                <?php if (isset($_GET['payment_month'])) { ?>
+                    <h2 class="mb-3">Transporter Report for <?php echo date('F Y', strtotime($_GET['payment_month'])); ?></h2>
                 <?php } else { ?>   
                     <h2 class="mb-3">Transporter Report</h2>
                 <?php } ?>
@@ -22,19 +19,19 @@ include('../navbar.php');
                 <form id="rotipayment" class="form-horizontal" method="GET"
                     action="<?php echo $_SERVER['PHP_SELF']; ?>" autocomplete="off">
                     <div class="mb-3 row">
-                        <label for="payment_week" class="col-3 control-label">Payment Week</label>
+                        <label for="payment_month" class="col-3 control-label">Payment Month</label>
                         <div class="col-6">
-                            <input type="week" class="form-control" name="payment_week" id="payment_week" placeholder="Payment Week" value="<?php echo (!empty($_GET['payment_week']) ? $_GET['payment_week'] : ''); ?>">
+                            <input type="month" class="form-control" name="payment_month" id="payment_month" placeholder="Payment Month" value="<?php echo (!empty($_GET['payment_month']) ? $_GET['payment_month'] : ''); ?>">
                         </div>
                         <div class="col-3 col-md-3">
                             <button class="btn btn-light" type="submit" name="search">Search</button>
                         </div>
                     </div>
                 </form>
-                <?php if (isset($_GET['payment_week'])) {
-                    list($year, $week) = explode('-W', $_GET['payment_week']);
-                    $start_date = date('Y-m-d', strtotime($year . 'W' . $week));
-                    $end_date = date('Y-m-d', strtotime($year . 'W' . $week . '+6 days'));
+                <?php if (isset($_GET['payment_month'])) {
+                    list($year, $month) = explode('-', $_GET['payment_month']);
+                    $start_date = date("$year-$month-01");
+                    $end_date = date("$year-$month-t");
                     $thali_count = mysqli_query($link, "SELECT `name`, SUM(`mini`) as total_mini, SUM(`small`) as total_small, SUM(`medium`) as total_medium, SUM(`large`) as total_large, SUM(`friday`) as total_friday, SUM(`barnamaj`) as total_barnamaj, SUM(`count`) as total_count FROM transporter_daily_count WHERE `date` BETWEEN '" . $start_date . "' AND '" . $end_date . "' GROUP BY `name`");
                     if ($thali_count->num_rows > 0) { ?>
                         <div class="table-responsive mb-3">
