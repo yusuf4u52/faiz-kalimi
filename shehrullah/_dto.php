dc9e2a960bb625c2128ad213cadc35e2b37993e9
hatimkamaal <hatim.kamaal@gmail.com>
2026-01-20 19:15:47 +0530

    query fixed for registartion report

--- shehrullah/_dto.php
+++ shehrullah/_dto.php
@@ -567,14 +567,16 @@ function get_registration_summary($hijri) {
     count(CASE WHEN a.attendance_type = "Y" and i.age < 12 and i.age > 4 THEN 1 ELSE NULL END) as kids,
     count(CASE WHEN a.attendance_type = "Y" and i.age < 5 THEN 1 ELSE NULL END) as infant,
     count(CASE WHEN a.attendance_type = "Y" THEN 1 ELSE NULL END) as attendees, 
-    count(CASE WHEN a.chair_preference = "Y" THEN 1 ELSE NULL END) as chairs,
-    t.pirsa_count,t.takhmeen, t.paid_amount, t.zabihat_count 
+    count(CASE WHEN a.chair_preference = "Y" THEN 1 ELSE NULL END) as chairs 
     FROM kl_shehrullah_attendees a
     JOIN its_data i ON i.its_id = a.its_id
-    JOIN kl_shehrullah_takhmeen t ON t.hof_id = a.hof_id    
-    WHERE t.year=? and a.year=? and t.takhmeen > 0
+    WHERE a.year=?
     GROUP BY a.hof_id    
     ) a;';
+
+    // ,    t.pirsa_count,t.takhmeen, t.paid_amount, t.zabihat_count
+    // JOIN kl_shehrullah_takhmeen t ON t.hof_id = a.hof_id    
+    // WHERE t.year=? and a.year=? and t.takhmeen >= 0
 
     $result = run_statement($query, $hijri, $hijri);
     if ($result->success && $result->count > 0) {
@@ -591,12 +593,15 @@ function get_registration_data($hijri)
     count(CASE WHEN a.attendance_type = "Y" and i.age < 12 and i.age > 4 THEN 1 ELSE NULL END) as kids,
     count(CASE WHEN a.attendance_type = "Y" and i.age < 5 THEN 1 ELSE NULL END) as infant,
     count(CASE WHEN a.attendance_type = "Y" THEN 1 ELSE NULL END) as attendees, 
-    count(CASE WHEN a.chair_preference = "Y" THEN 1 ELSE NULL END) as chairs,
-    t.pirsa_count,t.takhmeen, t.whatsapp, t.paid_amount, t.zabihat_count
+    count(CASE WHEN a.chair_preference = "Y" THEN 1 ELSE NULL END) as chairs
     FROM kl_shehrullah_attendees a
     JOIN its_data i ON i.its_id = a.its_id
-    JOIN kl_shehrullah_takhmeen t ON t.hof_id = a.hof_id   
-    WHERE t.year=? and a.year=? and t.takhmeen > 0 GROUP BY a.hof_id;';
+    WHERE a.year=? GROUP BY a.hof_id;';
+
+    //    JOIN kl_shehrullah_takhmeen t ON t.hof_id = a.hof_id   
+    // WHERE t.year=? and a.year=? and t.takhmeen > 0 GROUP BY a.hof_id;';
+    // ,t.pirsa_count,t.takhmeen, t.whatsapp, t.paid_amount, t.zabihat_count
+
 
     $result = run_statement($query, $hijri, $hijri);
     if ($result->success && $result->count > 0) {
