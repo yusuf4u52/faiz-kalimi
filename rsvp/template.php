576d3762edada563e88928ad392d385194404766
moizsahil5352 <moizlife@gmail.com>
2026-01-24 19:24:01 +0530

    add base url to styles and images.

--- rsvp/template.php
+++ rsvp/template.php
@@ -21,20 +21,20 @@ if (is_record_found($result)) {
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="/rsvp/assets/img/logo.avif" />
+    <link rel="shortcut icon" href="<?= getAppData('BASE_URI') ?>/assets/img/logo.avif" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Alice&family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css" />
-    <link rel="stylesheet" href="/rsvp/assets/css/main.css" />
+    <link rel="stylesheet" href="<?= getAppData('BASE_URI') ?>/assets/css/main.css" />
 </head>
 <body>
     <header class="header">
         <div class="container-fluid">
             <div class="row align-items-center">
                 <div class="col-4">
-                    <a href="<?= getAppData('BASE_URI') ?>"><img class="img-fluid" src="/rsvp/assets/img/logo.avif" alt="Miqaat RSVP (Kalimi Mohalla - Poona)" width="153" height="153" /></a>
+                    <a href="<?= getAppData('BASE_URI') ?>"><img class="img-fluid" src="<?= getAppData('BASE_URI') ?>/assets/img/logo.avif" alt="Miqaat RSVP (Kalimi Mohalla - Poona)" width="153" height="153" /></a>
                 </div>
                 <div class="col-8 text-end">
                     <h4><a href="<?= getAppData('BASE_URI') ?>">Miqaat RSVP <br/> (Kalimi Mohalla - Poona)</a></h4>
