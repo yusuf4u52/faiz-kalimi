30630549e4d18f0aedd8c4543efdb97780b55d82
hatimkamaal <hatim.kamaal@gmail.com>
2026-03-06 12:04:03 +0530

    Thaal count added

--- rsvp/views/report.php
+++ rsvp/views/report.php
@@ -3,7 +3,7 @@ function content_display()
 {
     $result = get_miqaat_stats_report();
     $data = $result->data;
-    $hdr = ['', 'Total', 'Mardo', 'Bairao', 'Kids', 'Miqaat', 'Start', 'End'];
+    $hdr = ['', 'Total', 'Total', 'Mardo', 'Bairao', 'Kids', 'Miqaat', 'Start', 'End'];
     $cols = ['total', 'mardo', 'bairo', 'infant', 'name', 'start_datetime', 'end_datetime'];
     ?>
     <h4 class="mb-3">Report</h5>
@@ -13,7 +13,8 @@ function content_display()
             echo '<thead><tr><th>' . implode('</th><th>', $hdr) . '</th></tr></thead>';
             echo '<tbody>';
                 foreach ($data as $row) {
-                    echo "<tr><td><a href='sectorwise_report/{$row['id']}'>Sectorwise</a></td>";
+                    $thaal = (int)($row["total"] / 8);
+                    echo "<tr><td><a href='sectorwise_report/{$row['id']}'>Sectorwise</a></td><td>$thaal</td>";
                     foreach ($cols as $col) {
                         echo "<td>{$row["$col"]}</td>";                        
                     }
@@ -24,4 +25,5 @@ function content_display()
         </table>
     </div>
     <?php
+
 } ?>
