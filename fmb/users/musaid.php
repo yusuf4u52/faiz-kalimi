48507eba6a63b9d1ffce2fef40247ad53e40eb42
moizsahil5352 <154581072+moizsahil5352@users.noreply.github.com>
2026-04-06 16:06:48 +0530

    Filter thalilist query to exclude hardstop entries

--- fmb/users/musaid.php
+++ fmb/users/musaid.php
@@ -32,7 +32,7 @@ if (isset($_SESSION['role']) && ($_SESSION['role'] === 'superadmin' || $_SESSION
 
 <div class="accordion" id="accordionMusaid">
 	<?php foreach ($musaid_list as $musaid) {
-		$result = mysqli_query($link, "SELECT * FROM thalilist where Previous_Due > 3 AND Transporter IS NOT NULL AND musaid='" . $musaid['email'] . "' order by `Paid %`");
+		$result = mysqli_query($link, "SELECT * FROM thalilist where Previous_Due > 3 AND Transporter IS NOT NULL AND musaid='" . $musaid['email'] . "' AND hardstop !=1 order by `Paid %`");
 		$thali_details = mysqli_fetch_all($result, MYSQLI_ASSOC);
 		$musaid_thali_count = count($thali_details);
 		if ($musaid_thali_count > 0) { ?>
