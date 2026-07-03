a32cc6074ce46439a8fbae759f6a587d9f1f870c
moizsahil5352 <154581072+moizsahil5352@users.noreply.github.com>
2026-07-03 15:16:11 +0530

    Fix SQL query and header redirection in stop_permanant.php

--- fmb/users/stop_permanant.php
+++ fmb/users/stop_permanant.php
@@ -8,10 +8,6 @@ $thali = $_POST['id'];
 $clearhub = true;
 
 if (isset($_POST['action']) && $_POST['action'] == 'stop_permanant') {
-	stoppermenant($thali, $clearhub);
-
-	function stoppermenant($thali, $clearhub)
-	{
 
 		$today = getTodayDateHijri();
 		$sql = "select id, (Previous_Due + yearly_hub + Zabihat - Paid) AS Total_Pending from thalilist WHERE id = '" . $thali . "'";
@@ -19,13 +15,12 @@ if (isset($_POST['action']) && $_POST['action'] == 'stop_permanant') {
 		$name = mysqli_fetch_assoc($result);
 
 		mysqli_query($link, "INSERT INTO change_table (`Thali`,`userid`, `Operation`, `Date`) VALUES ('" . $thali . "','" . $name['id'] . "', 'Stop Permanent','" . $today . "')") or die(mysqli_error($link));
-		mysqli_query($link, "UPDATE thalilist set Active='0', hardstop = 1,  WHERE id = '" . $name['id'] . "'") or die(mysqli_error($link));
+		mysqli_query($link, "UPDATE thalilist set Active = '0', hardstop = '1 ' WHERE id = '" . $name['id'] . "'") or die(mysqli_error($link));
 		if ($clearhub == "true") {
 			mysqli_query($link, "UPDATE thalilist set yearly_hub=yearly_hub - '" . $name['Total_Pending'] . "' WHERE id = '" . $name['id'] . "'") or die(mysqli_error($link));
 		}
 		mysqli_query($link, "update change_table set processed = 1 where userid = '" . $name['id'] . "' and `Operation` in ('New Thali') and processed = 0") or die(mysqli_error($link));
 
-		header("Location: /fmb/users/thalisearch.php??thalino=" . $_POST['thalino'] . "&tiffinno=" . $_POST['tiffinno'] . "&general=" . $_POST['general'] . "&year=" . $_POST['year'] . "&action=spermanant");
+		header("Location: /fmb/users/thalisearch.php?thalino=" . $_POST['thalino'] . "&tiffinno=" . $_POST['tiffinno'] . "&general=" . $_POST['general'] . "&year=" . $_POST['year'] . "&action=spermanant");
 		exit;
-	}
 }
