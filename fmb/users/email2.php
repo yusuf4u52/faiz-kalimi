6d3c431fd60070aa44f51c591cfb39cb493f1f59
moizsahil5352 <154581072+moizsahil5352@users.noreply.github.com>
2026-07-17 18:37:17 +0530

    Refactor email sending to use array for recipients

--- fmb/users/email2.php
+++ fmb/users/email2.php
@@ -26,8 +26,8 @@ if ($stop_thali->num_rows > 0) {
 
 			$email_subject = "Thali Stop Notification";
 			$email_body = "Salaam " . $list['NAME'] . ",<br><br>Your thali has been stopped from tomorrow till the date you selected in the FMB Website.<br><br> Note: If your thali is stopped by mistake, please whatsapp us on <a href='https://wa.me/919826932974' target='_blank'>+91 98269 32974</a><br><br>Thank you,<br>Kalimi Mohalla";
-			$email_to = $list['Email_ID'];
-			//sendEmail($email_to, $email_subject, $email_body, null, null, true);
+			$email_to = [ $list['Email_ID'] ];
+			sendEmail($email_to, $email_subject, $email_body, null, null, true);
 		}
 	}
 }
@@ -48,8 +48,8 @@ if ($chk_stop_thali->num_rows > 0) {
 
 				$email_subject = "Thali Start Notification";
 				$email_body = "Salaam " . $list['NAME'] . ",<br><br>Your thali has been started from tomorrow.<br><br>Note: If your thali is started by mistake or you wish to extend the period, please whatsapp us on <a href='https://wa.me/919826932974' target='_blank'>+91 98269 32974</a><br><br>Thank you,<br>Kalimi Mohalla";
-				$email_to = $list['Email_ID'];
-				//sendEmail($email_to, $email_subject, $email_body, null, null, true);
+				$email_to = [ $list['Email_ID'] ];
+				sendEmail($email_to, $email_subject, $email_body, null, null, true);
 			}
 		}
 	}
@@ -57,7 +57,6 @@ if ($chk_stop_thali->num_rows > 0) {
 
 $menu_item = mysqli_query($link, "SELECT `menu_item` FROM menu_list WHERE `menu_date` = '" . $tomorrow_date . "' AND `menu_type` = 'thaali'");
 if ($menu_item->num_rows > 0) {
-	echo 'test';
 	$sql = mysqli_query($link, "SELECT t.id, c.Thali, t.tiffinno, t.thalisize, t.NAME, t.CONTACT, t.Transporter,t.wingflat, t.society, t.Full_Address, c.Operation,c.id 
 			from change_table as c inner 
 			join thalilist as t on (c.userid = t.id) 
