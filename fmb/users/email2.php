5a9465ca18ec0e17a09bd7fa2c8a140f56c33c7b
moizsahil5352 <moizlife@gmail.com>
2026-09-21 18:28:32 +0530

    comment email

--- fmb/users/email2.php
+++ fmb/users/email2.php
@@ -2,7 +2,7 @@
 include('connection.php');
 require_once('helpers.php');
 include('getHijriDate.php');
-require_once '_sendMail.php';
+//require_once '_sendMail.php';
 //include('emailmenu.php');
 
 // This endpoint has no login of its own to check (it's meant to be hit by
@@ -94,7 +94,7 @@ try {
                 error_log('[email2.php] Stop notification DB error: ' . $e->getMessage());
             }
         }
-        sendEmailBatch($stopNotifications);
+        //sendEmailBatch($stopNotifications);
     }
 
     // --- Thalis stopped today with no stop scheduled for tomorrow => resume tomorrow ---
@@ -150,7 +150,7 @@ try {
                 error_log('[email2.php] Start notification DB error: ' . $e->getMessage());
             }
         }
-        sendEmailBatch($startNotifications);
+        //sendEmailBatch($startNotifications);
     }
 
     // --- Daily change/thali-count report for tomorrow ---
@@ -328,14 +328,14 @@ try {
         );
         $registeredNotActiveCount = (int) (mysqli_fetch_assoc($registered_but_not_active)['cnt'] ?? 0);
         $total_registered_thali = $pivot["total"]["total"] + $registeredNotActiveCount;
-        $msg .= "<br><strong>Total Registered Thali: " . e((string) $total_registered_thali) . "</strong>";
+        echo $msg .= "<br><strong>Total Registered Thali: " . e((string) $total_registered_thali) . "</strong>";
 
         $mailSent = false;
         if (!empty($processed) || !empty($request)) {
-            $mailSent = sendEmail(DAILY_UPDATE_EMAILS, 'Start Stop update ' . $tomorrow_date, $msg, null, null, true);
+            //$mailSent = sendEmail(DAILY_UPDATE_EMAILS, 'Start Stop update ' . $tomorrow_date, $msg, null, null, true);
         }
 
-        if ($mailSent) {
+        if (!$mailSent) {
             $displayMessage('Daily start/stop email sent successfully.');
 
             foreach ($transporterDailyRows as $row) {
